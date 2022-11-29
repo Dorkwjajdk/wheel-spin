0a74f29582e3edf4d089ad8193131e71bfdd596d
nifist99 <nikofigit@gmail.com>
2022-11-29 22:34:23 +0700

    oke

--- resources/views/web/base.blade.php
+++ resources/views/web/base.blade.php
@@ -245,6 +245,16 @@ let prizes = [
   @endforeach
 ];
 
+
+document.oncontextmenu = function (e) {e.preventDefault(); return false;}
+            document.onkeydown = function (e) {
+                if (event.keyCode == 123) {return false;}
+                if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {return false;}
+                if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {return false; }
+                if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {return false; }
+                if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {return false;}
+            }
+
 </script>
 <script src="{{url('assets/wheel/js/script.js')}}?version=1"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
